22c74dcd967dbe0f4da2d8406089f785e2197f7d
kip <himself@dennizvd.com>
2022-12-03 15:48:56 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,17 @@
 const { Events, ActivityType } = require('discord.js');
+const { default: mongoose } = require('mongoose');
+
+const { mongoURI } = require('../config.json');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		await mongoose.connect(mongoURI, { keepAlive: true });
+		
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		client.user.setActivity(`codump.github.io`, {
-			type: ActivityType.Watching, url: 'https://codump.github.io' });
+			type: ActivityType.Watching, url: 'https://codump.github.io'
+		});
 	},
 };
